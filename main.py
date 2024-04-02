636ab950fb30e1fbbd4c87f168b2e777d80c4565
INSEOP KIM <inseop0813@gmail.com>
2024-04-02 11:20:38 +0900

    create item orm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.users import router as user_router
+from routes.items import router as item_router
 
 app = FastAPI()
 
 app.include_router(user_router) # django => settings.py, urls.py
+app.include_router(item_router)
 
 if __name__ == "__main__":
     import uvicorn
--- schemas.py
+++ schemas.py
@@ -1,18 +1,28 @@
 from pydantic import BaseModel
 from typing import List
+from typing import Optional
 
 # schemas/item.py
 # schemas/user.py
 
 # pydantic -> 데이터 유효성 검증
-class Item(BaseModel):
-    id: int
-    owner_id: int
+class ItemBase(BaseModel):
     title: str
     description: str
 
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
     class Config:
         orm_mode = True # orm 방식으로 데이터 필드 읽기가 가능
+
+class ItemCreate(ItemBase):
+    pass
+
+class ItemUpdate(ItemBase):
+    title: Optional[str] = None
+    description: Optional[str] = None
 
 class UserBase(BaseModel):
     email: str
@@ -25,10 +35,15 @@ class User(UserBase):
         orm_mode = True
 
 class UserCreate(UserBase):
-    password: str
+    hashed_password: str
 
-from typing import Optional
 class UserUpdate(UserBase):
     # email: str | None = None # 3.10부터... | 등장.
     email: Optional[str] = None # 3.10부터... | 등장.
-    password: Optional[str] = None
+    hashed_password: Optional[str] = None
+
+# 그럼 api 만 잘 해도 잔고랑 뭐 다른건 공부 안해도 될까여,,
+# - 백엔드 신입 최소 조건, 역량 + Auth + DB (NoSQL)
+# - 면접 라이브 코딩 => 검색: 네이버 검색(기본 검색: 구글, bing)
+# 시작 연봉
+# REST API + DB(SQL) + AUTH + DB(NoSQL) + 기술(Chatting, Streaming .. )
